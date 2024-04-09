a2c6f6bf9069b7b6c1153fa6328ef51da4e4e193
Josué Estrada <dev.josuestrd@gmail.com>
2024-04-09 01:17:51 +0000

    Add section Sectores Estrategicos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import SliderImage from "./components/SliderImage";
 import fondoImage from "./public/fondo.jpg";
+import Image from 'next/image';
+import iconIndustria from '/icon_industria2.png';
+import iconMineria from '/public/icon_mineria1.png';
+import iconUrbano from '/public/icon_urbano1.png';
+import iconVial from '/public/icon_vial1.png';
+
+
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <SliderImage />
-      <section className="container flex flex-col justify-center bg-[url('/fondo.jpg')] bg-cover bg-center h-auto bg-no-repeat">
+      <section className="container flex flex-col justify-center bg-[url('/fondo.jpg')] bg-cover bg-center h-auto bg-no-repeat w-full">
         <div className="w-1/2 pl-24">
           <h2 className="text-4xl py-12 ">
             Soluciones Técnicas Industriales, Mineras, Urbanas y Viales
@@ -79,7 +87,58 @@ export default function Home() {
         </div>
       </section>
 
-      {/* SECCION SECTORES ESTRATEGIOS*/}
+      {/* SECCION SECTORES ESTRATEGICOS*/}
+      <section className="px-24 py-12 bg-[#f2f2f2] w-full">
+        <div className="text-center py-4">
+          <h2 className="font-bold text-4xl">SECTORES ESTRATÉGICOS</h2>
+          <p className="text-[#666]">
+            Garantizamos soluciones eficientes y rápidas, sencillas y menos costosas.
+            <br />
+            Sin perder de vista algo tan necesario e indispensable como la calidad y respeto a la ecología.
+          </p>
+        </div>
+        <div className="container flex justify-center w-auto gap-12">
+          <div className="flex flex-col items-center space-y-4">
+            <Image
+              src="/icon_industria2.png"
+              width={180}
+              height={180}
+              alt="Industria"
+            />
+            <h3 className="text-center">Industria
+            </h3>
+          </div>
+          <div className="flex flex-col items-center  space-y-4">
+            <Image
+              src="/icon_mineria1.png"
+              width={180}
+              height={180}
+              alt="Minería"
+            />
+            <h3 className="text-center">Minería</h3>
+          </div>
+          <div className="flex flex-col items-center space-y-4">
+            <Image
+              src="/icon_urbano1.png"
+              width={180}
+              height={180}
+              alt="Urbano"
+            />
+            <h3 className="text-center">
+              Urbano
+            </h3>
+          </div>
+          <div className="flex flex-col items-center space-y-4">
+            <Image
+              src="/icon_vial1.png"
+              width={180}
+              height={180}
+              alt="Víal"
+            />
+            <h3 className="text-center">Víal</h3>
+          </div>
+        </div>
+      </section>
 
       {/* SECCION CONTACTO*/}
     </main>
