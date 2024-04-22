9258e47c3f1ae830fdae01bec9137d9a47071849
Josué Estrada <dev.josuestrd@gmail.com>
2024-04-21 23:42:15 -0500

    install shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,15 @@
-import SliderImage from "./components/SliderImage";
+import Image from "next/image";
+import SliderImage from "../components/SliderImage";
 import fondoImage from "./public/fondo.jpg";
-import Image from 'next/image';
-import iconIndustria from '/icon_industria2.png';
-import iconMineria from '/public/icon_mineria1.png';
-import iconUrbano from '/public/icon_urbano1.png';
-import iconVial from '/public/icon_vial1.png';
-
-
+import iconIndustria from "/icon_industria2.png";
+import iconMineria from "/public/icon_mineria1.png";
+import iconUrbano from "/public/icon_urbano1.png";
+import iconVial from "/public/icon_vial1.png";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <SliderImage/>
+      <SliderImage />
       <section className="flex flex-col justify-center bg-[url('/fondo.jpg')] bg-cover bg-center h-auto bg-no-repeat w-full">
         <div className="w-1/2 pl-24">
           <h2 className="text-4xl py-12 ">
@@ -92,9 +90,11 @@ export default function Home() {
         <div className="text-center py-4">
           <h2 className="font-bold text-4xl">SECTORES ESTRATÉGICOS</h2>
           <p className="text-[#666]">
-            Garantizamos soluciones eficientes y rápidas, sencillas y menos costosas.
+            Garantizamos soluciones eficientes y rápidas, sencillas y menos
+            costosas.
             <br />
-            Sin perder de vista algo tan necesario e indispensable como la calidad y respeto a la ecología.
+            Sin perder de vista algo tan necesario e indispensable como la
+            calidad y respeto a la ecología.
           </p>
         </div>
         <div className="container flex justify-center w-auto gap-12">
@@ -105,8 +105,7 @@ export default function Home() {
               height={180}
               alt="Industria"
             />
-            <h3 className="text-center">Industria
-            </h3>
+            <h3 className="text-center">Industria</h3>
           </div>
           <div className="flex flex-col items-center  space-y-4">
             <Image
@@ -124,17 +123,10 @@ export default function Home() {
               height={180}
               alt="Urbano"
             />
-            <h3 className="text-center">
-              Urbano
-            </h3>
+            <h3 className="text-center">Urbano</h3>
           </div>
           <div className="flex flex-col items-center space-y-4">
-            <Image
-              src="/icon_vial1.png"
-              width={180}
-              height={180}
-              alt="Víal"
-            />
+            <Image src="/icon_vial1.png" width={180} height={180} alt="Víal" />
             <h3 className="text-center">Víal</h3>
           </div>
         </div>
@@ -145,15 +137,17 @@ export default function Home() {
         <div className="text-center text-white bg-[url('/fondocontacto.jpg')] w-auto bg-no-repeat bg-cover">
           <div className="bg-[rgba(12,12,12,0.55)] w-full h-[400px] flex justify-center items-center flex-col gap-6">
             <h2 className="font-bold text-4xl">Cuéntenos su proyecto</h2>
-          <p className="">
-            Consulte con nosotros por la fabricación de tanques sedimentadores, clarificadores, aireadores, sistemas de flotación por oxígeno disuelto (D.A.F.), desnatadores, etc.
-          </p>
-          <button className="border-white border-2 p-2 rounded-sm">Contactar</button> </div>
-          
+            <p className="">
+              Consulte con nosotros por la fabricación de tanques
+              sedimentadores, clarificadores, aireadores, sistemas de flotación
+              por oxígeno disuelto (D.A.F.), desnatadores, etc.
+            </p>
+            <button className="border-white border-2 p-2 rounded-sm">
+              Contactar
+            </button>{" "}
+          </div>
         </div>
       </section>
-
-
     </main>
   );
 }
